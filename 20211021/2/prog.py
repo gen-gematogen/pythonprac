a72f969dc545f0b5dfc80779bf036b1732a9d119
gen-gematogen <shutkov6914@gmail.com>
2021-10-22 10:45:04 +0300

    Finished homework for last task, proud of myself

--- 20211021/2/prog.py
+++ 20211021/2/prog.py
@@ -2,14 +2,17 @@ from math import *
 
 s = input().split(' ')
 functions = dict()
+lines_count = 1
 
 while s[0] != 'quit':
     if s[0][0] == ':':
         # it's a function
-        functions[s[0][1:]] = (s[1], s[2])
+        functions[s[0][1:]] = (s[1:-1], s[-1])
     else:
         ev = dict()
-        ev[functions[s[0]][0]] = eval(s[1])
+        for i in range(len(s[1:])):
+            ev[functions[s[0]][0][i]] = eval(s[i + 1])
         print(eval(functions[s[0]][1], None, ev))
     s = input().split(' ')
-print(len(functions) + 1)
+    lines_count += 1
+print(eval(s[1] + '.format(' + str(len(functions) + 1) + ',' + str(lines_count) + ')'))
--- 20211021/3/prog.py
+++ 20211021/3/prog.py
@@ -1,14 +1,28 @@
 from collections import defaultdict
 
 word_len = int(input())
-s = input()
 word_count = defaultdict(lambda: 0)
-while s:
-    s = ''.join([i for i in s if i.isalpha() or i == ' '])
-    print(s)
+text = []
+while True:
+    try:
+        s = input()
+    except:
+        break
+    s = ''.join([i if i.isalpha() else ' ' for i in s])
     s = s.lower().split()
     for word in s:
         if len(word) == word_len:
             word_count[word] += 1
-    s = input()
-print(*[i[1] for i in sorted(zip(word_count.values(), word_count.keys()))], sep = ' ')
+
+max_occ = 0
+common_words = []
+
+for k in word_count:
+    if word_count[k] > max_occ:
+        common_words = [k]
+        max_occ = word_count[k]
+    elif word_count[k] == max_occ:
+        common_words.append(k)
+        
+if common_words: 
+    print(*common_words)
