8c33c6daf9899b395f4149889c5e3d410b89fde2
gen-gematogen <shutkov6914@gmail.com>
2021-11-01 16:59:55 +0300

    Finished homework, tested all tasks with checker

--- 20211028/2/task2.py
+++ 20211028/2/task2.py
@@ -3,8 +3,8 @@ import sys
 
 def slide(seq, n):
     i = 0
-    while True:
-        yield from itertools.islice(seq, i, i + n)
+    for it in itertools.tee(seq, len(seq)):
+        yield from itertools.islice(it, i, i + n)
         i += 1
 
 exec(sys.stdin.read())
