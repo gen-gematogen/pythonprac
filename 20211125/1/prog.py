ef654cfcf9aa3127effa6823dcdfb6cf6b8b1748
gen-gematogen <shutkov6914@gmail.com>
2021-12-01 22:38:43 +0300

    Finally got that task with encodeings and struct lib usage

--- 20211125/1/prog.py
+++ 20211125/1/prog.py
@@ -2,10 +2,8 @@ import sys
 
 data = sys.stdin.buffer.readline()
 n = data[0]
-#data = data[1:]
 l = len(data) - 1
 k = n
-print(l)
 slices = []
 L = 1
 while L < l:
@@ -13,19 +11,8 @@ while L < l:
     slices.append(data[L:L+sz])
     k -= 1
     L += sz
-    #L = i*l//k + 1
-    #R = (i+1)*l//k + 1
-    #slices.append(data[L: R])
-    #k -= 1
-    #l -= len(slices[-1])
-    #print(L, R, R - L)
 
 slices.sort()
-
-k = 0
-for i in slices:
-    print(i, k)
-    k += 1
 
 sys.stdout.buffer.write(data[:1])
 for i in slices:
