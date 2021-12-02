8f637ee9fb6dae6582d20ca2714703e8d594b55b
gen-gematogen <shutkov6914@gmail.com>
2021-12-02 15:58:04 +0300

    Python 3.10 functionality tested

---
+++ 20211202/1/prog.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+class dump(type):
+    def __init__(self, name, parents, ns):
+        for method_name in ns:
+            method = ns[method_name]
+            if callable(method):
+                @wraps(method)
+                def new_method(*args, method = method, method_name = method_name, **kwargs):
+                    print(f'{method_name}: {args[1:]}, {kwargs}')
+                    return method(*args, **kwargs)
+                setattr(self, method_name, new_method)
+        return super().__init__(name, parents, ns)
