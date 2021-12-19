b74bba43e3108b6e8c610675c1f5efb8f6031e51
gen-gematogen <shutkov6914@gmail.com>
2021-12-19 21:54:39 +0300

    Bug fixed

--- 20211125/1/prog.py
+++ 20211125/1/prog.py
@@ -6,13 +6,6 @@ l = len(data) - 1
 k = n
 slices = []
 
-#L = 1
-#while L < l:
-#    sz = (l - L) // k
-#    slices.append(data[L:L+sz])
-#    k -= 1
-#    L += sz
-
 for i in range(n):
     slices.append(data[1 + i * l // n: 1 + (i + 1) * l // n])
 
