03112b835013299781511f8383bb8094790bddeb
gen-gematogen <shutkov6914@gmail.com>
2021-12-19 20:51:51 +0300

    Bug fix

--- 20211125/1/prog.py
+++ 20211125/1/prog.py
@@ -1,16 +1,20 @@
 import sys
 
-data = sys.stdin.buffer.readline()
+data = sys.stdin.buffer.read()
 n = data[0]
 l = len(data) - 1
 k = n
 slices = []
-L = 1
-while L < l:
-    sz = (l - L) // k
-    slices.append(data[L:L+sz])
-    k -= 1
-    L += sz
+
+#L = 1
+#while L < l:
+#    sz = (l - L) // k
+#    slices.append(data[L:L+sz])
+#    k -= 1
+#    L += sz
+
+for i in range(n):
+    slices.append(data[1 + i * l // n: 1 + (i + 1) // n])
 
 slices.sort()
 
