a7331baa0925ccd77702efc50774cc21eabf2b17
gen-gematogen <shutkov6914@gmail.com>
2022-02-20 11:24:12 +0300

    List all branches of repository
    If you launch ptogram without parametres, it lists all repo branches.
    Needed to run file from root of repo.

--- 20220214/1/prog2.py
+++ 20220214/1/prog2.py
@@ -3,20 +3,27 @@ import sys
 from glob import iglob
 from os.path import join
 
-repo = sys.argv[1]
-repopath = join(repo, "objects", "??", "*")
-for objname in iglob(repopath):
-    print(objname)
+if len(sys.argv) == 1:
+    repopath = join(".git", "refs", "heads", "*")
+    for i, branch in enumerate(iglob(repopath)):
+        print(i + 1, ": ", branch, sep = '')
 
-    with open(objname, "rb") as objfile:
-        fullobj = zlib.decompress(objfile.read())
-        header, _, body = fullobj.partition(b'\x00')
-        kind, size = header.split()
-        print(kind.decode(), int(size))#, body)
-        if kind == b"commit":
-            print(body.decode())
-        elif kind == b"tree":
-            while body:
-                treehdr, _, tail = body.partition(b'\x00')
-                gitid, body = body[:20], body[20:]
-                print(f"\t{treehdr}, {gitid.hex()}")
+else:
+    repo = sys.argv[1]
+    repopath = join(repo, "objects", "??", "*")
+    print(repopath)
+    for objname in iglob(repopath):
+        print(objname)
+
+        with open(objname, "rb") as objfile:
+            fullobj = zlib.decompress(objfile.read())
+            header, _, body = fullobj.partition(b'\x00')
+            kind, size = header.split()
+            print(kind.decode(), int(size))#, body)
+            if kind == b"commit":
+                print(body.decode())
+            elif kind == b"tree":
+                while body:
+                    treehdr, _, tail = body.partition(b'\x00')
+                    gitid, body = body[:20], body[20:]
+                    print(f"\t{treehdr}, {gitid.hex()}")
