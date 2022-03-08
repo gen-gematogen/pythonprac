83ffc34934f19e1e0e747d9f808b49c2d2ff9972
gen-gematogen <shutkov6914@gmail.com>
2022-03-08 19:57:51 +0300

    2.3

--- 20220228/prog.py
+++ 20220228/prog.py
@@ -1,8 +1,12 @@
 import textdistance
 
 def dist(s1, s2, s3):
-    return textdistance.Levenshtein(s1, s2)
-
+    if s3 == "L":
+        return textdistance.Levenshtein(s1, s2)
+    elif s3 == "D":
+        return textdistance.damerau_levenstein(s1, s2)
+    return -1
+    
 s = input()
 s1, s2 = s.split()[:2]
 s3 = input()
